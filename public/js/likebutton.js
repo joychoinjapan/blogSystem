677459c7ece26c179c592ac9f892e75a5b657f52
miaobaobao <joychoinjapan@gmail.com>
2018-04-06 13:14:32 +0900

    fix bugs on like button working on cloud server

--- public/js/likebutton.js
+++ public/js/likebutton.js
@@ -4,13 +4,20 @@ $.ajaxSetup({
     }
 });
 
+
+var url = location.href;
+var index=url.indexOf("user");
+var newUrl=url.substr(0,index-1);
+
+
+
 $(".like-button").click(function (event) {
     var target=$(event.target);
     var current_like=target.attr('like-value');
     var user_id=target.attr('like-user');
     if(current_like==1){
         $.ajax({
-            url:"/user/"+user_id+"/unfan",
+            url:newUrl+"/user/"+user_id+"/unfan",
             method:"POST",
             dataType:"json",
             success:function (data) {
@@ -19,14 +26,14 @@ $(".like-button").click(function (event) {
                     return;
                 }
                 target.attr("like-value",0);
-                target.text("关注")
+                target.text("お気に入り登録")
             }
 
         })
     }else{
         $.ajax({
             contentType:"application/x-www-form-urlencoded",
-            url:"/user/"+user_id+"/fan",
+            url:newUrl+"/user/"+user_id+"/fan",
             method:"POST",
             dataType:"json",
             success:function (data) {
@@ -35,7 +42,7 @@ $(".like-button").click(function (event) {
                     return;
                 }
                 target.attr("like-value",1);
-                target.text("取消关注")
+                target.text("フォローを取り消す")
             }
 
         })
